3bb80ac0e5961083d57d0ec308aaec568ab6ec53
wrlakshan <rashmika.lakshan@ebuilder.com>
2024-08-06 19:09:17 +0530

    create cdn using npm construct

--- bin/cdk-s3-cdn.ts
+++ bin/cdk-s3-cdn.ts
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
 import { CdkS3CdnStack } from '../lib/cdk-s3-cdn-stack';
 
+const S3WebsiteProps =  {
+    projectName: "my-project",
+    s3WebSiteDeploySource: './assert',
+    websiteIndexDocuments: 'index.html',
+    websiteErrorDocuments: 'index.html',
+    cdnWebsiteIndexDocuments: 'index.html',
+    cdnComment: 'CDN for static website application',
+  }
+
+  
 const app = new cdk.App();
-new CdkS3CdnStack(app, 'CdkS3CdnStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new CdkS3CdnStack(app, 'CdkS3CdnStack', S3WebsiteProps);
--- lib/cdk-s3-cdn-stack.ts
+++ lib/cdk-s3-cdn-stack.ts
@@ -1,16 +1,27 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3sw from "@wrlakshan/cdk-s3-cdn-construct";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export interface CdkS3CdnStackProp extends cdk.StackProps {
+  projectName: string;
+  s3WebSiteDeploySource: string;
+  websiteIndexDocuments: string;
+  websiteErrorDocuments: string;
+  cdnWebsiteIndexDocuments: string;
+  cdnComment: string;
+}
 
 export class CdkS3CdnStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: CdkS3CdnStackProp) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkS3CdnQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const s3website = new s3sw.CdkS3CdnConstruct(this, "s3-website", {
+      projectName: props.projectName,
+      s3WebSiteDeploySource: props.s3WebSiteDeploySource,
+      websiteIndexDocuments: props.websiteIndexDocuments,
+      websiteErrorDocuments: props.websiteErrorDocuments,
+      cdnWebsiteIndexDocuments: props.cdnWebsiteIndexDocuments,
+      cdnComment: props.cdnComment,
+    });
   }
 }
